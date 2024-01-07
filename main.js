89fbc310006dd4a590d1cdd78707ef6606961091
Vince <wjs531@gmail.com>
2024-01-07 21:52:05 +0900

    regexp

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log(123)
+let str = `
+011-4235-2343
+Thewdsd@gmail.com
+https://naver.com/?s=frozen
+abbcccdddd
+the quick brown fox jumps over the lazy dog.
+http:localhost:1234
+`
+
+
+console.log(str.match(/\w{2,3}/g))
